280c6bc4d06d373fc67d9d29e15e4000a345de99
tereshchenkomax <tereshchenkomax@gmail.com>
2017-01-02 13:52:46 +0200

    no message

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+    //echo "Hello world!";
+    //echo time();
+
+    $a = $_GET["a"];
+    $b = $_GET["b"];
+
+    echo "a+b=".($a+$b);
+
+    for($i=0;$i<10;$i++){
+        echo $i."<br>";
+    }
+ ?>
