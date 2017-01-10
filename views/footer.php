287ff6290cf9f0375e82b0c405d50c24aabe9c53
tereshchenkomax <tereshchenkomax@gmail.com>
2017-01-10 23:26:46 +0200

    no message

--- views/footer.php
+++ views/footer.php
@@ -1,5 +1,6 @@
 <footer class="text-center">
     <p>Блог для задания<br>Copyright&copy;2017</p></footer>
+<script src="http://code.jquery.com/jquery-latest.js"></script>
 <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
 <script src="lib/owl/dist/owl.carousel.min.js"></script>
 </body>
